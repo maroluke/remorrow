be161203434ceef873a8cbbdcb99fec3be5948b8
maroluke <hello@markolukac.ch>
2023-03-17 12:22:30 +0100

    fix target links

--- src/components/ContactIcons.vue
+++ src/components/ContactIcons.vue
@@ -8,25 +8,25 @@ import { RouterLink } from "vue-router";
 import { useNavigationStore } from "@/stores/navigation";
 
 export default {
-    name: "ContactIcons",
-    components: {
-        EmailIcon,
-        LinkedinIcon,
-        InstagramIcon,
-        IconLogo,
-        RouterLink,
-        TwitterIcon,
-    },
-    data() {
-        return {
-            navigationLinks: [],
-            contactIcons: {},
-            navigationState: {},
-        };
-    },
-    mounted() {
-        this.navigationState = useNavigationStore();
-    },
+	name: "ContactIcons",
+	components: {
+		EmailIcon,
+		LinkedinIcon,
+		InstagramIcon,
+		IconLogo,
+		RouterLink,
+		TwitterIcon,
+	},
+	data() {
+		return {
+			navigationLinks: [],
+			contactIcons: {},
+			navigationState: {},
+		};
+	},
+	mounted() {
+		this.navigationState = useNavigationStore();
+	},
 };
 </script>
 
@@ -35,32 +35,46 @@ export default {
 		<!-- <RouterLink to="/" @click="navigationState.toggleNavigation">
 			<IconLogo class="fill-silver w-20" />
 		</RouterLink> -->
-        <!-- <p class="mb-0 text-gray-500 font-light text-base">Kontakt</p> -->
-        
-        <ul class="contact-icons flex justify-start gap-8 flex-grow">
+		<!-- <p class="mb-0 text-gray-500 font-light text-base">Kontakt</p> -->
+
+		<ul class="contact-icons flex justify-start gap-8 flex-grow">
 			<li>
-				<RouterLink to="#" class="text-silver">
-                    <EmailIcon class="h-5 text-inherit hover:fill-rich-electric-blue" />
-                </RouterLink>
+				<RouterLink to="mailto:hey@42mrrw.net" class="text-silver">
+					<EmailIcon
+						class="h-5 text-inherit hover:fill-rich-electric-blue"
+					/>
+				</RouterLink>
 			</li>
 
-            <li>
-                <a href="https://twitter.com/42mrrw" class="text-silver">
-                    <TwitterIcon class="h-5 text-inherit hover:fill-rich-electric-blue" />
-                </a>
-            </li>
+			<li>
+				<a href="https://twitter.com/42mrrw"
+                target="_blank" class="text-silver">
+					<TwitterIcon
+						class="h-5 text-inherit hover:fill-rich-electric-blue"
+					/>
+				</a>
+			</li>
 
-            <li>
-                <a href="https://linkedin.com/company/42mrrw/" class="text-silver">
-                    <LinkedinIcon class="h-5 text-inherit hover:fill-rich-electric-blue" />
-                </a>
-            </li>
+			<li>
+				<a
+					href="https://linkedin.com/company/42mrrw/"
+                    target="_blank"
+					class="text-silver"
+				>
+					<LinkedinIcon
+						class="h-5 text-inherit hover:fill-rich-electric-blue"
+					/>
+				</a>
+			</li>
 
-            <li>
-                <a href="https://instagram.com/42mrrw/" class="text-silver">
-                    <InstagramIcon class="h-5 text-inherit hover:fill-rich-electric-blue" />
-                </a>
-            </li>
+			<li>
+				<a href="https://instagram.com/42mrrw/"
+                target="_blank" class="text-silver">
+					<InstagramIcon
+						class="h-5 text-inherit hover:fill-rich-electric-blue"
+					/>
+				</a>
+			</li>
 		</ul>
 	</div>
 </template>
