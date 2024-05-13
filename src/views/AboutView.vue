273f7e009d63fa217aa0cea85b6ad77d9fa6748c
Marko Lukac <hello@markolukac.ch>
2024-05-13 11:24:22 +0200

    add hover effect to profile pictures

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,46 @@
-<script setup>
+<script>
 import TitleParagraphItem from "@components/TitleParagraphItem.vue";
 import JumboHeader from "@components/JumboHeader.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	components: {
+		TitleParagraphItem,
+		JumboHeader,
+	},
+	// data() {
+	// 	return {
+	// 	};
+	// },
+	methods: {
+		markText(id, event) {
+			// get the element where this function was triggered
+			const eventTarget = event.target;
+
+			const element = document.getElementById(id);
+
+			// if the element has the class "mark" already, do nothing
+			if (element.classList.contains("mark")) {
+				return;
+			} else {
+				// remove the class "mark" from all elements
+				document.querySelectorAll(".mark").forEach((element) => {
+					element.classList.remove("mark");
+				});
+
+				// add the class "mark" to the element
+				element.classList.add("mark");
+				eventTarget.classList.add("mark");
+			}
+		},
+
+		demarkText() {
+			document.querySelectorAll(".mark").forEach((element) => {
+				element.classList.remove("mark");
+			});
+		},
+	},
+});
 </script>
 
 <template>
@@ -32,10 +72,10 @@ import JumboHeader from "@components/JumboHeader.vue";
 						<p>Wir arbeiten kollektiv am Aufbau von re:morrow und suchen dabei unseren eigenen Weg.</p>
 						<p>Perspektivisch bauen wir eine digitale und globale Plattform zur Unterstützung von nachhaltigen <RouterLink class="glossar-link" to="/projekte/">Ideen, Projekten und Startups</RouterLink> auf. Gleichzeitig entwickeln wir <strong>eine <RouterLink class="glossar-link" to="/community/">Community</RouterLink> mit Menschen, die Ideen mit positiver Wirkung aktiv unterstützen</strong>.</p>
 						<div class="flex gap-5 mb-5 items-center justify-between">
-							<p class="mb-0 flex-1">Die treibenden Kräfte bei uns sind <strong>Sirik</strong> und <strong>Marko</strong>.</p>
+							<p class="mb-0 flex-1">Die treibenden Kräfte bei uns sind <strong id="sirik" class="transition-all duration-300">Sirik</strong> und <strong id="marko" class="transition-all duration-300">Marko</strong>.</p>
 							<div class="flex-1 flex justify-end">
-								<img src="@/assets/media/profile-sirik.jpg" alt="Sirik Loosman" class="-rotate-3 mt-10 z-20 block rounded-full w-24 h-24 object-cover border-4 border-white md:w-32 md:h-32" />
-								<img src="@/assets/media/profile-marko.jpg" alt="Marko Lukac" class="rotate-6 -ml-5 z-10 block rounded-full w-24 h-24 object-cover border-4 border-white md:w-32 md:h-32" />
+								<img @mouseenter="markText('sirik', $event)" @mouseleave="demarkText()" @click="markText('sirik', $event)" src="@/assets/media/profile-sirik.jpg" alt="Sirik Loosman" class="z-20 transition-all duration-300 -rotate-3 mt-10 z-20 block rounded-full w-24 h-24 object-cover border-4 border-white md:w-32 md:h-32" />
+								<img @mouseenter="markText('marko', $event)" @mouseleave="demarkText()" @click="markText('marko', $event)" src="@/assets/media/profile-marko.jpg" alt="Marko Lukac" class="z-10 transition-all duration-300 rotate-6 -ml-5 z-10 block rounded-full w-24 h-24 object-cover border-4 border-white md:w-32 md:h-32" />
 							</div>
 						</div>
 						<p>Du willst uns kennenlernen? Für einen Schwatz sind wir immer zu haben!</p>
@@ -122,3 +162,13 @@ import JumboHeader from "@components/JumboHeader.vue";
         <div class="bg-gradient-to-b from-black via-transparent to-transparent w-full h-full"></div>
     </div> -->
 </template>
+
+<style scoped>
+	.mark {
+		@apply text-rich-electric-blue inline-block;
+	}
+
+	img.mark {
+		@apply border-rich-electric-blue z-30;
+	}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -70,6 +70,6 @@ module.exports = {
 			});
 		}),
 
-		require("flowbite/plugin"),
+		// require("flowbite/plugin"),
 	],
 };
