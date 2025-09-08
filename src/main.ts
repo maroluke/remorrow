b3b2d5ae23c043d8f0738676e7e42e24f6267d8c
Marko Lukac <marko.lukac@clustertec.com>
2025-09-08 21:44:53 +0200

    add utm tracking

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,37 @@ import router from "./router";
 
 import "@assets/main.css";
 
+// UTM Parameter Tracking für Plausible
+function trackUTMParameters() {
+	const urlParams = new URLSearchParams(window.location.search);
+	const utmParams = {};
+	
+	// UTM Parameter sammeln
+	['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'].forEach(param => {
+		if (urlParams.has(param)) {
+			utmParams[param] = urlParams.get(param);
+		}
+	});
+	
+	// An Plausible senden
+	if (Object.keys(utmParams).length > 0) {
+		// @ts-ignore
+		if (window.plausible) {
+			// @ts-ignore
+			window.plausible('UTM Parameters', {
+				props: utmParams
+			});
+		}
+	}
+}
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
+// UTM Tracking nach dem Mount
 app.mount("#app");
+
+// UTM Parameter tracken
+trackUTMParameters();
